1a185b0978ab0cec59d2bde00d8b0c3cee8f0d02
jingliu8876133 <jingliu8876133@gmail.com>
2023-11-19 22:54:17 +0800

    add print statement policy

--- src/policy_manager/policy.py
+++ src/policy_manager/policy.py
@@ -1,6 +1,6 @@
 from typing import Tuple
 
-from action_runner.action import Action, SlotFillingAction, SmartHomeOperatingAction
+from action_runner.action import Action, SlotFillingAction, SmartHomeOperatingAction, PrintStatementAction
 from action_runner.rag_action import RAGAction
 from conversation_tracker.context import ConversationContext
 from gm_logger import get_logger
@@ -77,3 +77,24 @@ class RAGPolicy(Policy):
                 return True, RAGAction(model_type, possible_slots)
 
         return False, None
+    
+
+class PrintStatementPolicy(Policy):
+
+    def __init__(self, prompt_manager: PromptManager):
+        Policy.__init__(self, prompt_manager)
+
+    def handle(self, intent: IntentWithEntity, context: ConversationContext, model_type: str) -> Tuple[bool, Action]:
+        if intent.intent.name == "查询打印回单":
+            possible_slots = {entity.possible_slot.name: entity.possible_slot for entity in intent.entities if entity.possible_slot}
+            if len(possible_slots) == 1:
+                return True, PrintStatementAction(
+                    f'已经帮你打印好时间范围为{possible_slots["时间范围"].value}的回单了')
+            # elif len(possible_slots) == 3:
+            #     return True, PrintStatementAction(f'已经帮你{possible_slots["操作"].value}{possible_slots["位置"].value}的{possible_slots["对象"].value}到{possible_slots["操作值"].value}了')
+            # elif len(possible_slots) == 4:
+            #     return True, PrintStatementAction(f'已经帮你{possible_slots["操作"].value}{possible_slots["位置"].value}的{possible_slots["对象"].value}到{possible_slots["操作值"].value}了')
+            else:
+                return False, None
+        else:
+            return False, None
