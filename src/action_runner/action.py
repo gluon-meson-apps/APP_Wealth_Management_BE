bac70ffce968744c8f1052639788b5ce0d5626fe
jingliu8876133 <jingliu8876133@gmail.com>
2023-11-21 10:25:03 +0800

    add actions

--- src/action_runner/action.py
+++ src/action_runner/action.py
@@ -45,6 +45,42 @@ class PrintStatementAction(Action):
         context.set_status('action:print_statement')
         logger.debug("成功查询打印了回单")
         return ActionResponse(text=self.response)
+    
+class FontPageAdjustmentAction(Action):
+    def __init__(self, response):
+        self.response = response
+
+    def run(self, context) -> ActionResponse:
+        context.set_status('action:font_page_adjustment')
+        logger.debug("成功调整了页面字体")
+        return ActionResponse(text=self.response)
+    
+class AdjustTableColumnAction(Action):
+    def __init__(self, response):
+        self.response = response
+
+    def run(self, context) -> ActionResponse:
+        context.set_status('action:adjust_table_column')
+        logger.debug("成功调整了表头")
+        return ActionResponse(text=self.response)
+    
+class ActivateFunctionAction(Action):
+    def __init__(self, response):
+        self.response = response
+
+    def run(self, context) -> ActionResponse:
+        context.set_status('action:activate_function')
+        logger.debug("成功开通了功能")
+        return ActionResponse(text=self.response)
+    
+class QAAction(Action):
+    def __init__(self, response):
+        self.response = response
+
+    def run(self, context) -> ActionResponse:
+        context.set_status('action:qa')
+        logger.debug("成功跳转到小招X")
+        return ActionResponse(text=self.response)
 
 
 class ChitChatAction(Action):
