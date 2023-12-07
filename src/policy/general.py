7684edbef833a66f20e1ba4067a9a45a69020b83
hyjiang <hongyu.jiang@thoughtworks.com>
2023-12-07 15:26:26 +0800

    [Hongyu] jump out if intent got skill_irrelevant or other_skill

--- src/policy/general.py
+++ src/policy/general.py
@@ -17,6 +17,8 @@ SLOT_SIG_TRH = 0.9
 
 MAX_FOLLOW_UP_TIMES = 2
 
+BUSINESS_INTENS = ["activate_function", "add_header", "enlarge_page", "reduce_page", "page_resize", "remove_header"]
+
 class EndDialoguePolicy(Policy):
     def __init__(self, prompt_manager: PromptManager, form_store: FormStore):
         Policy.__init__(self, prompt_manager)
@@ -38,7 +40,7 @@ class IntentFillingPolicy(Policy):
         if IE.intent is None:
             context.set_state("intent_filling")
             return True, IntentFillingAction(prompt_manager=self.prompt_manager)
-        elif IE.intent.confidence < INTENT_SIG_TRH:
+        elif IE.intent.confidence < INTENT_SIG_TRH and IE.intent.name in BUSINESS_INTENS:
             context.set_state("intent_confirm")
             return True, IntentConfirmAction(IE.intent, prompt_manager=self.prompt_manager)        
         return False, None
@@ -69,7 +71,7 @@ class AssistantPolicy(Policy):
         possible_slots = self.get_possible_slots(intent=IE)
         logger.debug(f"最终识别的\n意图：{IE.intent.name}\n实体：{[f'{slot.name}: {slot.value}'for slot in possible_slots if slot]}")
         if form := self.form_store.get_form_from_intent(IE.intent):
-            if IE.intent.name not in ["skill_irrelevant", "other_skill", "chitchat"]:
+            if IE.intent.name in BUSINESS_INTENS:
                 print(f'exec action {form.action}')
                 return True, BankRelatedAction(form.action, possible_slots)
             elif IE.intent.name in ["skill_irrelevant", "other_skill"]:
