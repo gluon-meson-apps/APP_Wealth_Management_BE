e2742dab1aa5141b4da78d89e424e7efd106558c
Dan Chen <dchen@thoughtworks.com>
2023-12-26 14:11:34 +0800

    [Dan Chen] #N/A: fix tests

--- tests/output_adapter/test_slot_normalize.py
+++ tests/output_adapter/test_slot_normalize.py
@@ -7,57 +7,57 @@ from output_adapter.base import BaseOutputAdapter, NormalizeType
 input_and_expected_value = [
     [
         '百分之5',
-        NormalizeType.PERCENTAGE,
+        'font_change',
         '10',
     ],
     [
         '百分之4',
-        NormalizeType.PERCENTAGE,
+        'font_change',
         '10',
     ],
     [
         '百分之15',
-        NormalizeType.PERCENTAGE,
+        'font_change',
         '20',
     ],
     [
         '百分之14',
-        NormalizeType.PERCENTAGE,
+        'font_change',
         '20',
     ],
     [
         '百分之五',
-        NormalizeType.PERCENTAGE,
+        'font_change',
         '10',
     ],
     [
         '5%',
-        NormalizeType.PERCENTAGE,
+        'font_change',
         '10',
     ],
     [
         '倒数第二列',
-        NormalizeType.NUMBER,
+        'header_position',
         '-2',
     ],
     [
         '第三列',
-        NormalizeType.NUMBER,
+        'header_position',
         '3',
     ],
     [
         '倒数三',
-        NormalizeType.NUMBER,
+        'header_position',
         '-3',
     ],
     [
         '倒数3',
-        NormalizeType.NUMBER,
+        'header_position',
         '-3',
     ],
     [
         '月消费',
-        NormalizeType.STRING,
+        'header_position',
         '月消费'
     ]
 ]
@@ -67,7 +67,7 @@ class TestNormalizePercentage(unittest.TestCase):
     @parameterized.expand(input_and_expected_value)
     def test_normalize_slot_value(self, input, normalize_type, expected_value):
         adapter = BaseOutputAdapter()
-        result = adapter.normalize_slot_value(input, normalize_type)
+        result = adapter.normalize_slot_value(input, normalize_type, "test_normalize")
         assert result == expected_value
 
 
