6ec563ae80987a4677a2b1196875d0c60209fee8
Vivi Huang <rwhuang@thoughtworks.com>
2024-01-10 15:44:42 +0800

    [vivihuang][TG] *66 fix: temp mock response for hsbc api.

--- src/third_system/hsbc_connect_api.py
+++ src/third_system/hsbc_connect_api.py
@@ -1,12 +1,13 @@
-import requests
-
-
 class HsbcConnectApi:
     def __init__(self):
         self.base_url = "https://hkl20146575.hc.cloud.hk.hsbc:25000/PaymentRulesValidator/Report"
 
     def validate_file(self, file) -> str:
         if file:
-            response = requests.post(self.base_url, files={"Attachment": file}, verify=False)
-            return response.text
+            # todo: currently mock response here
+            return """
+            <body><div id="dummycontext"></div><div id="dummystatus"></div><div id="dummysummary"></div></body>
+            """
+            # response = requests.post(self.base_url, files={"Attachment": file}, verify=False)
+            # return response.text
         raise FileNotFoundError("No file valid.")
