82634e35c3eece563c506c086b171a8d5ce161c4
Vivi Huang <rwhuang@thoughtworks.com>
2024-01-10 18:52:35 +0800

    [vivihuang][TG] *66 fix: will upload validatioin result file.

--- src/action/actions/tb_guru/file_validation.py
+++ src/action/actions/tb_guru/file_validation.py
@@ -4,6 +4,9 @@ from action.base import Action, ChatResponseAnswer, ResponseMessageType, General
 from gluon_meson_sdk.models.scenario_model_registry.base import DefaultScenarioModelRegistryCenter
 from third_system.hsbc_connect_api import HsbcConnectApi
 from third_system.search_entity import SearchResponse, SearchItem
+from third_system.unified_search import UnifiedSearch
+
+report_filename = "file_validation_report.html"
 
 prompt = """
 ## Role
@@ -12,12 +15,12 @@ You are an assistant with name as "TB Guru", you need to answer the user's query
 ## Reference info
 Do NOT actually try to do file validation because the file validation is already done by another system.
 BUT PRETEND that you did the file validation.
-The URL to download the validation report is attached below.
-The validation report is a html file and the file name is "File validation report.html".
+The URL to download the validation report is attached below and the validation report is a html file.
 Tell the user to download the report to check the validation details.
 
-## Validation report file url
-{file_url}
+## Validation report file info
+File name: {report_filename}
+File url: {file_url}
 
 ## User query
 {user_input}
@@ -37,12 +40,21 @@ def get_first_file_content(context) -> str:
 
 class FileValidation(Action):
     def __init__(self):
+        self.unified_search = UnifiedSearch()
         self.scenario_model_registry = DefaultScenarioModelRegistryCenter()
         self.scenario_model = self.get_name() + "_action"
         self.hsbc_connect_api = HsbcConnectApi()
 
     def get_name(self) -> str:
         return "file_validation"
+
+    def _upload_file(self, res: str) -> str:
+        file = (report_filename, res, "text/html")
+        files = [("files", file)]
+        links = self.unified_search.upload_file_to_minio(files)
+        if links and links[0]:
+            return links[0]
+        raise Exception("Upload file failed.")
 
     def run(self, context):
         chat_model = self.scenario_model_registry.get_model(self.scenario_model)
@@ -51,11 +63,14 @@ class FileValidation(Action):
         file_content_text: str = get_first_file_content(context)
 
         try:
-            # todo: upload validation result and return download link here
-            self.hsbc_connect_api.validate_file(file_content_text)
-            download_link = "https://test.com/file_validation_report.html"
+            res = self.hsbc_connect_api.validate_file(file_content_text)
+            download_link = self._upload_file(res)
 
-            final_prompt = prompt.format(user_input=context.conversation.current_user_input, file_url=download_link)
+            final_prompt = prompt.format(
+                user_input=context.conversation.current_user_input,
+                file_url=download_link,
+                report_filename=report_filename,
+            )
             logger.info(f"final prompt: {final_prompt}")
             result = chat_model.chat(final_prompt, max_length=1024).response
             logger.info(f"chat result: {result}")
--- src/third_system/hsbc_connect_api.py
+++ src/third_system/hsbc_connect_api.py
@@ -1,3 +1,18 @@
+import os
+
+import requests
+from requests import Response
+
+
+def mock_validate_res():
+    mock_response = Response()
+    mock_response.status_code = 200
+    mock_response._content = """
+    <body><div id="dummycontext"></div><div id="dummystatus"></div><div id="dummysummary"></div></body>
+    """.encode()
+    return mock_response
+
+
 class HsbcConnectApi:
     def __init__(self):
         self.base_url = "https://hkl20146575.hc.cloud.hk.hsbc:25000/PaymentRulesValidator/Report"
@@ -5,9 +20,11 @@ class HsbcConnectApi:
     def validate_file(self, file) -> str:
         if file:
             # todo: currently mock response here
-            return """
-            <body><div id="dummycontext"></div><div id="dummystatus"></div><div id="dummysummary"></div></body>
-            """
-            # response = requests.post(self.base_url, files={"Attachment": file}, verify=False)
-            # return response.text
+            response = (
+                mock_validate_res()
+                if os.getenv("LOCAL_MODE") == "1"
+                else requests.post(self.base_url, files={"Attachment": file}, verify=False)
+            )
+            if response.status_code == 200:
+                return response.text
         raise FileNotFoundError("No file valid.")
