c7c12b53f3e236d5b2c1d7a4ff1bc6e1ead42a75
Vivi Huang <rwhuang@thoughtworks.com>
2024-01-12 18:44:43 +0800

    [vivihuang][TG] *67 fix: search for latest and current data to generate ppt.

--- src/action/actions/tb_guru/wcs_data_query.py
+++ src/action/actions/tb_guru/wcs_data_query.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import uuid
+from typing import Union, Any
 
 import pandas as pd
 from gluon_meson_sdk.models.scenario_model_registry.base import DefaultScenarioModelRegistryCenter
@@ -16,7 +17,6 @@ from action.base import (
 )
 from third_system.search_entity import SearchItem, SearchParam
 from third_system.unified_search import UnifiedSearch
-from utils.action_helper import format_entities_for_search
 from utils.ppt_helper import plot_graph, ppt_generation
 
 report_filename = "file_validation_report.html"
@@ -64,17 +64,21 @@ class WcsDataQuery(Action):
     def get_name(self) -> str:
         return "wcs_data_query"
 
-    def _generate_ppt(self, data: list[SearchItem], insight: str) -> str:
+    def _generate_ppt(
+        self, current_company_data: list[SearchItem], all_companies_data: list[SearchItem], insight: str
+    ) -> str:
         file_dir = f"{self.tmp_file_dir}/{str(uuid.uuid4())}"
         os.makedirs(file_dir, exist_ok=True)
         image_paths = [f"{file_dir}/image1.png", f"{file_dir}/image2.png"]
         ppt_path = f"{file_dir}/ppt.pptx"
-        df = pd.DataFrame([w.model_dump() for w in data])
-        plot_graph(df, "wcs_data_query", "days", image_paths[0])
-        plot_graph(df, "wcs_data_query", "days", image_paths[1])
-        df = df.drop(columns=["days", "meta__score", "meta__reference", "id"])
+        df_current = pd.DataFrame([w.model_dump() for w in current_company_data])
+        df_all = pd.DataFrame([w.model_dump() for w in all_companies_data])
+        company_name = current_company_data[0].company
+        plot_graph(df_current, f"{company_name} – Working Capital Metrics Trend", "days", image_paths[0])
+        plot_graph(df_all, f"Peer Comparison ({all_companies_data[0].days})", "company", image_paths[1])
+        df_all = df_all.drop(columns=["days", "meta__score", "meta__reference", "id"])
         ppt_generation(
-            df, llm_insight=insight, company_name=data[0].company, image_paths=image_paths, output_path=ppt_path
+            df_all, llm_insight=insight, company_name=company_name, image_paths=image_paths, output_path=ppt_path
         )
         files = [
             ("files", ("tb_guru_ppt.pptx", open(ppt_path, "rb"), UploadFileContentType.PPTX)),
@@ -83,33 +87,41 @@ class WcsDataQuery(Action):
         shutil.rmtree(file_dir)
         return links[0] if links else ""
 
+    async def _search(self, context) -> tuple[Union[list[SearchItem], list[Any]], Union[list[SearchItem], list[Any]]]:
+        entity_dict = context.conversation.get_simplified_entities()
+        query_list = [
+            f"""Query WCS data with days as {entity_dict["days"]}:"""
+            if "days" in entity_dict
+            else "Query WCS data sort by days descending:"
+        ]
+        if "company" in entity_dict:
+            query_list.append(f"""Query WCS data with company {entity_dict["company"]}:""")
+        search_res = [self.unified_search.search(SearchParam(query=q)) for q in query_list]
+        items = [s[0].items if s else [] for s in search_res]
+        all_companies_data, current_company_data = items[0], items[1] if len(items) > 1 else []
+        latest_period = all_companies_data[0].days if all_companies_data else ""
+        return list(
+            filter(lambda s: s.days == latest_period, all_companies_data)
+        ) if latest_period else [], current_company_data
+
     async def run(self, context) -> ActionResponse:
         chat_model = self.scenario_model_registry.get_model(self.scenario_model)
         logger.info(f"exec action: {self.get_name()} ")
 
-        entities_without_ppt = format_entities_for_search(context.conversation, ["is_ppt_output"])
-        search_res = self.unified_search.search(
-            SearchParam(
-                query=f"""
-        Query WCS data with below fields:
-        {entities_without_ppt}
-        """
-            )
-        )
+        is_ppt_output = context.conversation.get_simplified_entities()["is_ppt_output"]
 
-        wcs_data = search_res[0].items
+        all_companies_data, current_company_data = await self._search(context)
 
         final_prompt = prompt.format(
             user_input=context.conversation.current_user_input,
-            wcs_data="\n".join([item.model_dump_json() for item in wcs_data]),
+            wcs_data="\n".join([item.model_dump_json() for item in current_company_data + all_companies_data]),
         )
         logger.info(f"final prompt: {final_prompt}")
         result = chat_model.chat(final_prompt, max_length=1024).response
         logger.info(f"chat result: {result}")
 
-        is_ppt_output = context.conversation.get_simplified_entities()["is_ppt_output"]
-        if is_ppt_output and wcs_data:
-            ppt_link = self._generate_ppt(wcs_data, result)
+        if is_ppt_output and current_company_data and all_companies_data:
+            ppt_link = self._generate_ppt(current_company_data, all_companies_data, result)
             final_prompt = ppt_prompt.format(ppt_link=ppt_link)
             logger.info(f"ppt prompt: {final_prompt}")
             result = chat_model.chat(final_prompt, max_length=1024).response
