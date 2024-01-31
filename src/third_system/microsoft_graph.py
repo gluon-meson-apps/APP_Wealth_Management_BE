b0e079b4c581b20a7e9e1c19a90e08b435ccb634
Vivi Huang <rwhuang@thoughtworks.com>
2024-01-31 15:23:06 +0800

    [vivihuang][TG] *97 fix: refactor graph api.

--- src/third_system/microsoft_graph.py
+++ src/third_system/microsoft_graph.py
@@ -36,6 +36,13 @@ class EmailHandlingFailedException(Exception):
         super().__init__(self.message)
 
 
+class TokenExpiredException(Exception):
+    def __init__(self):
+        self.message = "Token expired"
+        self.error_type = "TOKEN_EXPIRED"
+        super().__init__(self.message)
+
+
 class Graph:
     def __init__(self):
         self.config = {
@@ -48,16 +55,14 @@ class Graph:
         self.access_token = self.get_access_token()
         self.inbox_folder_id, self.archive_folder_id = self.list_folders()
 
-    def get_access_token(self, grant_type="client_credentials") -> str:
+    def get_access_token(self) -> str:
         try:
             data = {
-                "grant_type": grant_type,
+                "grant_type": "client_credentials",
                 "client_id": self.config["client_id"],
                 "client_secret": self.config["client_secret"],
                 "scope": "https://graph.microsoft.com/.default",
             }
-            if grant_type == "refresh_token":
-                data["refresh_token"] = self.config["access_token"]
             response = requests.post(
                 f'https://login.microsoftonline.com/{self.config["tenant_id"]}/oauth2/v2.0/token',
                 data=data,
@@ -71,7 +76,7 @@ class Graph:
             raise EmailHandlingFailedException(str(err), "GET_TOKEN_FAILED")
 
     def refresh_access_token(self):
-        self.access_token = self.get_access_token(grant_type="refresh_token")
+        self.access_token = self.get_access_token()
 
     def check_token_expired(self, response):
         data = parse_json_response(response)
@@ -80,132 +85,98 @@ class Graph:
             self.refresh_access_token()
         return result
 
-    def list_folders(self):
-        endpoint = f"{self.user_api_endpoint}/mailFolders"
+    def call_graph_api(self, endpoint: str, method: str = "GET", data: dict = None):
         headers = {"Authorization": "Bearer " + self.access_token}
         try:
-            response = requests.get(endpoint, headers=headers)
+            response = (
+                requests.get(endpoint, headers=headers)
+                if method == "GET"
+                else requests.post(endpoint, headers=headers, json=data)
+            )
             response.raise_for_status()
-            data = response.json()
-            values = data["value"] if "value" in data and data["value"] else []
-            inbox_folder = next(filter(lambda v: v.get("displayName") in ["收件箱", "Inbox"], values), None)
-            archive_folder = next(filter(lambda v: v.get("displayName") in ["存档", "Archive"], values), None)
-            return inbox_folder.get("id") if inbox_folder else "", archive_folder.get("id") if archive_folder else ""
+            data = parse_json_response(response) if response.status_code == 200 else {}
+            return data["value"] if "value" in data and data["value"] else []
         except HTTPError as http_err:
             if self.check_token_expired(http_err.response):
-                return self.list_folders()
+                raise TokenExpiredException()
             else:
                 logger.error(f"HTTP error occurred: {http_err}")
-                raise EmailHandlingFailedException(http_err.response.text, "LIST_FOLDER_FAILED")
+                raise EmailHandlingFailedException(http_err.response.text, "CALL_API_FAILED")
         except Exception as err:
             logger.error(f"Other error occurred: {err}")
-            raise EmailHandlingFailedException(str(err), "LIST_FOLDER_FAILED")
+            raise EmailHandlingFailedException(str(err), "CALL_API_FAILED")
+
+    def list_folders(self):
+        try:
+            data = self.call_graph_api(f"{self.user_api_endpoint}/mailFolders")
+            inbox_folder = next(filter(lambda v: v.get("displayName") in ["收件箱", "Inbox"], data), None)
+            archive_folder = next(filter(lambda v: v.get("displayName") in ["存档", "Archive"], data), None)
+            return inbox_folder.get("id") if inbox_folder else "", archive_folder.get("id") if archive_folder else ""
+        except TokenExpiredException:
+            return self.list_folders()
 
     def get_first_inbox_message(self) -> Union[Email, None]:
-        fields_query = "$select=id,conversationId,subject,sender,body,hasAttachments,receivedDateTime"
-        order_query = "$orderby=receivedDateTime asc"
-        endpoint = (
-            f"{self.user_api_endpoint}/mailFolders/{self.inbox_folder_id}/messages?{fields_query}&{order_query}&$top=1"
-        )
-        headers = {"Authorization": "Bearer " + self.access_token}
         try:
-            response = requests.get(endpoint, headers=headers)
-            response.raise_for_status()
-            data = response.json()
-            values = data["value"] if "value" in data and data["value"] else []
-            first_message = next(iter(values), None)
+            fields_query = "$select=id,conversationId,subject,sender,body,hasAttachments,receivedDateTime"
+            order_query = "$orderby=receivedDateTime asc"
+            endpoint = f"{self.user_api_endpoint}/mailFolders/{self.inbox_folder_id}/messages?{fields_query}&{order_query}&$top=1"
+            data = self.call_graph_api(endpoint)
+            first_message = next(iter(data), None)
             return parse_email(first_message) if first_message else None
-        except HTTPError as http_err:
-            if self.check_token_expired(http_err.response):
-                return self.get_first_inbox_message()
-            else:
-                logger.error(f"HTTP error occurred: {http_err}")
-                raise EmailHandlingFailedException(http_err.response.text, "GETTING_NEW_EMAIL_FAILED")
-        except Exception as err:
-            logger.error(f"Other error occurred: {err}")
-            raise EmailHandlingFailedException(str(err), "GETTING_NEW_EMAIL_FAILED")
+        except TokenExpiredException:
+            return self.get_first_inbox_message()
 
     def list_attachments(self, message_id):
-        endpoint = f"{self.user_api_endpoint}/messages/{message_id}/attachments"
-        headers = {"Authorization": "Bearer " + self.access_token}
-
         try:
-            response = requests.get(endpoint, headers=headers)
-            response.raise_for_status()
-            data = response.json()
-            values = data["value"] if "value" in data and data["value"] else []
-            return [v for v in values if "contentBytes" in v and v["contentBytes"]]
-        except HTTPError as http_err:
-            if self.check_token_expired(http_err.response):
-                return self.list_attachments(message_id)
-            else:
-                logger.error(f"HTTP error occurred: {http_err}")
-                raise EmailHandlingFailedException(http_err.response.text, "LIST_ATTACHMENTS_FAILED")
-        except Exception as err:
-            logger.error(f"Other error occurred: {err}")
-            raise EmailHandlingFailedException(str(err), "LIST_ATTACHMENTS_FAILED")
+            endpoint = f"{self.user_api_endpoint}/messages/{message_id}/attachments"
+            data = self.call_graph_api(endpoint)
+            return [v for v in data if "contentBytes" in v and v["contentBytes"]]
+        except TokenExpiredException:
+            return self.list_attachments(message_id)
 
     def send_email(self, email: Email, answer: str, attachments: list[EmailAttachment] = None):
-        endpoint = f"{self.user_api_endpoint}/sendMail"
-        headers = {"Authorization": "Bearer " + self.access_token}
-        message = {
-            "subject": f"[TB Guru Reply] {email.subject}",
-            "body": {"contentType": "html", "content": answer},
-            "toRecipients": [{"emailAddress": {"address": email.sender.address}}],
-        }
-        if attachments:
-            message["attachments"] = [
-                {
-                    "@odata.type": "#microsoft.graph.fileAttachment",
-                    "name": a.name,
-                    "contentType": a.type,
-                    "contentBytes": a.bytes,
-                }
-                for a in attachments
-            ]
-            message["hasAttachments"] = True
-
         try:
-            response = requests.post(
+            endpoint = f"{self.user_api_endpoint}/sendMail"
+            message = {
+                "subject": f"[TB Guru Reply] {email.subject}",
+                "body": {"contentType": "html", "content": answer},
+                "toRecipients": [{"emailAddress": {"address": email.sender.address}}],
+            }
+            if attachments:
+                message["attachments"] = [
+                    {
+                        "@odata.type": "#microsoft.graph.fileAttachment",
+                        "name": a.name,
+                        "contentType": a.type,
+                        "contentBytes": a.bytes,
+                    }
+                    for a in attachments
+                ]
+                message["hasAttachments"] = True
+
+            self.call_graph_api(
                 endpoint,
-                headers=headers,
-                json={
+                method="POST",
+                data={
                     "message": message,
                     "saveToSentItems": "true",
                 },
             )
-            response.raise_for_status()
             logger.info(f"Send email to {email.sender.address} successfully.")
             self.archive_email(email)
-        except HTTPError as http_err:
-            if self.check_token_expired(http_err.response):
-                self.send_email(email, answer, attachments)
-            else:
-                logger.error(f"HTTP error occurred: {http_err}")
-                raise EmailHandlingFailedException(http_err.response.text, "SEND_EMAIL_FAILED")
-        except Exception as err:
-            logger.error(f"Other error occurred: {err}")
-            raise EmailHandlingFailedException(str(err), "SEND_EMAIL_FAILED")
+        except TokenExpiredException:
+            self.send_email(email, answer, attachments)
 
     def archive_email(self, email: Email):
-        endpoint = f"{self.user_api_endpoint}/mailFolders/{self.inbox_folder_id}/messages/{email.id}/move"
-        headers = {"Authorization": "Bearer " + self.access_token}
         try:
-            response = requests.post(
+            endpoint = f"{self.user_api_endpoint}/mailFolders/{self.inbox_folder_id}/messages/{email.id}/move"
+            self.call_graph_api(
                 endpoint,
-                headers=headers,
-                json={
+                method="POST",
+                data={
                     "destinationId": self.archive_folder_id,
                 },
             )
-            response.raise_for_status()
             logger.info("Archive email successfully.")
-        except HTTPError as http_err:
-            if self.check_token_expired(http_err.response):
-                self.archive_email(email)
-            else:
-                logger.error(f"HTTP error occurred: {http_err}")
-                raise EmailHandlingFailedException(http_err.response.text, "ARCHIVE_EMAIL_FAILED")
-        except Exception as err:
-            logger.error(f"Other error occurred: {err}")
-            raise EmailHandlingFailedException(str(err), "ARCHIVE_EMAIL_FAILED")
+        except TokenExpiredException:
+            self.archive_email(email)
