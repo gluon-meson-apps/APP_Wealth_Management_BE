83227372fc11f7a285888498cf296f95e794fcf1
ran_lin <boydfd@163.com>
2024-02-04 22:04:20 +0800

    [rlin] *N/A feat: enhance e2e generator.

--- tests/e2e/e2e_util.py
+++ tests/e2e/e2e_util.py
@@ -5,6 +5,6 @@ def get_log_id_filter(specific_log_ids: list[str]):
     log_id_filter = ""
     if specific_log_ids:
         log_ids = [f"'{log_id}'" for log_id in specific_log_ids]
-        log_test_ids = [f"'test__e2e_test__{log_id}'" for log_id in specific_log_ids]
+        log_test_ids = [f"'test__e2e_test___{log_id}'" for log_id in specific_log_ids]
         log_id_filter = f" and log_id in ({','.join(log_ids + log_test_ids)})"
     return log_id_filter
