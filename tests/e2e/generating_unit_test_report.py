f7192c8033198dd573a9ec59a5948f124ffb33f1
zhenqi.wang <zhenqi.wang@thoughtworks.com>
2024-02-09 18:15:53 +0800

    [zhenqi][hsbc_tb_guru] *98 feature: add data transforming and combining functions; adapt to async main functions; use command line tool to run the script; update jinja template to return params json string.

--- tests/e2e/generating_unit_test_report.py
+++ tests/e2e/generating_unit_test_report.py
@@ -1,13 +1,25 @@
+import asyncio
 import importlib
 import json
 import os
 import re
 
+import click
 import pandas as pd
 
 package = "generated_golden_test_set"
-report_file_name = f"testing_report.xlsx"
+report_file_path = f"{os.path.dirname(os.path.abspath(__file__))}/testing_report.xlsx"
 unit_test_file_name_pattern = r"^unit_test_\w+_unit.py"
+MAX_NUMBER_OF_TESTS = 1024
+
+
+@click.group()
+@click.option("-p", "--path", default=report_file_path, show_default=True,
+              help="Specify the path of the generated report file", required=False)
+@click.pass_context
+def cli(ctx, path):
+    ctx.ensure_object(dict)
+    ctx.obj["PATH"] = path
 
 
 def form_the_dataframe(results: list[tuple]) -> pd.DataFrame:
@@ -26,7 +38,7 @@ def execute_test(file_path):
         module_name = get_module_name(file_path, package)
         module = importlib.import_module(module_name)
         if hasattr(module, "main") and callable(module.main):
-            return module.main()
+            return asyncio.run(module.main())
         else:
             raise Exception(f"The module {module_name} does not have a main function or it's not callable")
     except Exception as e:
@@ -34,7 +46,7 @@ def execute_test(file_path):
         raise Exception(str(e))
 
 
-def traverse_test_cases(test_files_list):
+def traverse_and_execute_test_cases(test_files_list):
     test_results = []
 
     for test_file in test_files_list:
@@ -49,23 +61,37 @@ def file_name_is_valid(file_name, pattern):
     return re.match(pattern, file_name) is not None
 
 
-def get_unit_test_files_list(test_cases_root_folder):
-    return [
-        os.path.join(root, file)
-        for root, _, files in os.walk(test_cases_root_folder)
-        for file in files if file_name_is_valid(file, unit_test_file_name_pattern)
-    ]
+def get_unit_test_files_list(test_cases_root_folder, count=MAX_NUMBER_OF_TESTS):
+    result = []
+    for root, _, files in os.walk(test_cases_root_folder):
+        for file in files:
+            if file_name_is_valid(file, unit_test_file_name_pattern) and count:
+                result.append(os.path.join(root, file))
+                if count:
+                    count -= 1
+                else:
+                    return result
+    return result
 
 
-def record_testing_details():
-    files_list = get_unit_test_files_list(f"{os.path.dirname(os.path.abspath(__file__))}/{package}")
-    files_list = files_list[:2]
+@cli.command(name="record_tests",
+             help="Run all the unit test and record the testing details")
+@click.pass_context
+@click.option("-c", "--count", type=click.INT, default=MAX_NUMBER_OF_TESTS,
+              help="Number of test cases to be run. this will be helpful "
+                   "if you only want to verify the process, running all test cases would cost much")
+@click.argument("test_cases_root_folder",
+                type=click.Path(),
+                required=False,
+                default=f"{os.path.dirname(os.path.abspath(__file__))}/generated_golden_test_set/")
+def record_testing_details(ctx, count, test_cases_root_folder):
+    files_list = get_unit_test_files_list(test_cases_root_folder, count=count)
 
-    results = traverse_test_cases(files_list)
+    results = traverse_and_execute_test_cases(files_list)
 
     df = form_the_dataframe(results)
 
-    with pd.ExcelWriter(f"{os.path.dirname(os.path.abspath(__file__))}/{report_file_name}",
+    with pd.ExcelWriter(ctx.obj["PATH"],
                         engine="openpyxl") as writer:
         df.to_excel(writer, sheet_name="details", index=False)
     return df
@@ -102,21 +128,31 @@ def extract_keys(json_str):
 
 
 def unnest_json_values(original_df, extract_column):
-    new_columns = original_df[extract_column].apply(pd.Series)
+    new_columns = original_df[extract_column].apply(lambda x: json.loads(x)).apply(pd.Series)
 
     final_df = pd.concat([original_df, new_columns], axis=1)
 
     final_df.drop(extract_column, axis=1, inplace=True)
 
-    print(final_df.columns)
     return final_df
 
 
-def generate_report_from_testing_details(dataframe):
-    flattened_df = unnest_json_values(dataframe, "params")
-    report_df = statistic_testing_result(flattened_df)
+def get_testing_details_dataframe_from_excel(path):
+    df = pd.read_excel(path, sheet_name="details")
+    df = df.fillna("unknown")
+    return df
 
-    with pd.ExcelWriter(f"{os.path.dirname(os.path.abspath(__file__))}/{report_file_name}",
+
+@cli.command(name="report",
+             help="Generate a report of the testing details")
+@click.pass_context
+def generate_report_from_testing_details(ctx):
+    dataframe = get_testing_details_dataframe_from_excel(ctx.obj["PATH"])
+    flattened_df = unnest_json_values(dataframe, "params")
+    statistic_df = statistic_testing_result(flattened_df)
+    report_df = statistic_df.groupby(["scenario", "use_case"]).sum().sort_index()
+
+    with pd.ExcelWriter(report_file_path,
                         engine="openpyxl",
                         mode="a") as writer:
         report_df.to_excel(writer, sheet_name="summary", index=False)
@@ -124,10 +160,5 @@ def generate_report_from_testing_details(dataframe):
     return report_df
 
 
-def main():
-    df = record_testing_details()
-    generate_report_from_testing_details(df)
-
-
 if __name__ == "__main__":
-    main()
+    cli(obj={})
--- tests/e2e/test_generating_unit_test_report.py
+++ tests/e2e/test_generating_unit_test_report.py
@@ -1,3 +1,4 @@
+import asyncio
 from unittest.mock import patch, MagicMock
 
 import pandas as pd
@@ -6,13 +7,15 @@ import pytest
 import tests.e2e.generating_unit_test_report as report
 
 """
-- Make the program as a command line script
-    - Use the package `click` for bootstrapping the commands
-    - Parameters
-        - report_file_path
-    - [Command] unit_test
-    - [Command] report
-- Traverse the test case directory and execute all unit tests
+- [x] Make the program as a command line script
+    - [x] Use the package `click` for bootstrapping the commands
+    - [x] [Option] path - Specify the path of the generated report file, default: $project_root/tests/e2e/testing_report.xlsx
+    - [x] [Command] record_tests
+        - [x] [Option] count - Number of test cases to be run, this will be helpful if you \
+                       only want to verify the process, running all test cases would cost much
+        - [x] [Argument] TEST_CASES_ROOT_FOLDER - The folder of the test cases, defaults to $project_root/tests
+    - [x] [Command] report
+- [ ] Traverse the test case directory and execute all unit tests
     - [x] Traverse the test case directory
         - [x] Filter out all unit tests
         - [x] Get the module name from the test case directory
@@ -20,8 +23,12 @@ import tests.e2e.generating_unit_test_report as report
         - [x] Receive the result of the main() function
         - [x] Put the results to a Pandas DataFrame
         - [ ] [TODO] Find out how to judge whether the test passes or fails
+        - [ ] **[ATTENTION] The main() function of the tests are async, when being executed, \
+              it will raise an exception saying \
+              "TypeError: object ChatModelResponse can't be used in 'await' expression"**
     - [x] Write the DataFrame to a specific sheet of an excel file
-- Generate a report from the above testing details
+- [ ] Generate a report from the above testing details
+    - [x] Get the dataframe firstly
     - [x] Group the DataFrame by the scenario
         - [x] Count the number of passed and failed tests
         - [x] Add columns `Positive results` and `Negative results`, fill in the counts
@@ -29,6 +36,8 @@ import tests.e2e.generating_unit_test_report as report
     - [x] Split the items in the `params` column to new columns
         - [ ] [TODO] Decide how to find the model type
     - [x] Write the new DataFrame to another sheet of the same excel file
+    - [ ] [TODO] Create a map for use case
+    - [x] Group the report by scenario and use case
 """
 
 
@@ -62,6 +71,23 @@ class TestTraverseTestCaseDirectoryAndExecuteTests:
         expected_files = []
         assert result == expected_files
 
+    def test_given_a_count_number_should_return_the_number_of_files_in_the_list(self, tmp_path):
+        test_dir = tmp_path / "test_directory"
+        test_dir.mkdir()
+        sub_dir1 = test_dir / "subfolder1"
+        sub_dir1.mkdir()
+        file1 = sub_dir1 / "unit_test_file1_unit.py"
+        file1.write_text("File 1 content")
+        file2 = sub_dir1 / "unit_test_file2_unit.py"
+        file2.write_text("File 2 content")
+        file3 = sub_dir1 / "unit_test_file3_unit.py"
+        file3.write_text("File 3 content")
+        file4 = sub_dir1 / "unit_test_file4_unit.py"
+        file4.write_text("File 4 content")
+
+        result = report.get_unit_test_files_list(test_dir, count=3)
+        assert len(result) == 3
+
     def test_given_a_file_path_should_return_its_module_name(self):
         file_path = "/path/to/package/subfolder1/unit_test_something_unit.py"
         package_name = "package"
@@ -78,7 +104,10 @@ class TestTraverseTestCaseDirectoryAndExecuteTests:
             mock_get_module_name
     ):
         mock_module = mock_import_module.return_value
-        mock_module.main.return_value = "Test Result"
+
+        async def mock_coroutine():
+            return "Test Result"
+        mock_module.main = MagicMock(return_value=mock_coroutine())
 
         result = report.execute_test("/path/to/package/subfolder1/unit_test_something_unit.py")
 
@@ -114,21 +143,25 @@ class TestTraverseTestCaseDirectoryAndExecuteTests:
 
         mock_execute_test.side_effect = expected_results
 
-        result = report.traverse_test_cases(test_files_list)
+        result = report.traverse_and_execute_test_cases(test_files_list)
         assert result == expected_results
 
 
 class TestGenerateReportFromTestingDetails:
+    def test_should_get_dataframe_from_testing_details_excel_file(self):
+        # report.get_testing_details_dataframe_from_excel("/tmp/test.xlsx")
+        assert True
+
     def test_given_test_details_should_return_a_expended_dataframe_containing_testing_results(self):
         original_columns = ["use_case", "scenario", "prompt", "params", "llm_result", "test_result"]
         original_data = [
-            ("use_case_1", "scenario_1", "prompt_1_1", {"temperature": 0, "top_p": 0.7, "jsonable": True},
+            ("use_case_1", "scenario_1", "prompt_1_1", "{\"temperature\": 0, \"top_p\": 0.7, \"jsonable\": true}",
              "llm_result_1_1", "pass"),
-            ("use_case_1", "scenario_2", "prompt_1_2", {"temperature": 0.9, "top_p": 0.8, "jsonable": True},
+            ("use_case_1", "scenario_2", "prompt_1_2", "{\"temperature\": 0.9, \"top_p\": 0.8, \"jsonable\": true}",
              "llm_result_1_2", "fail"),
-            ("use_case_2", "scenario_1", "prompt_2_1", {"temperature": 0, "top_p": 0.8, "jsonable": True},
+            ("use_case_2", "scenario_1", "prompt_2_1", "{\"temperature\": 0, \"top_p\": 0.8, \"jsonable\": true}",
              "llm_result_2_1", "pass"),
-            ("use_case_2", "scenario_2", "prompt_2_2", {"temperature": 0.8, "top_p": 0.9, "jsonable": True},
+            ("use_case_2", "scenario_2", "prompt_2_2", "{\"temperature\": 0.8, \"top_p\": 0.9, \"jsonable\": true}",
              "llm_result_2_2", "good"),
         ]
         original_df = pd.DataFrame(data=original_data, columns=original_columns)
@@ -137,16 +170,16 @@ class TestGenerateReportFromTestingDetails:
                             "fail", "pass", "unknown", "pass percentage", "fail percentage"]
         expected_data = [
             ["use_case_1", "scenario_1", "prompt_1_1",
-             {"temperature": 0, "top_p": 0.7, "jsonable": True}, "llm_result_1_1",
+             "{\"temperature\": 0, \"top_p\": 0.7, \"jsonable\": true}", "llm_result_1_1",
              "pass", 0.0, 1.0, 0.0, 100.0, 0.0],
             ["use_case_1", "scenario_2", "prompt_1_2",
-             {"temperature": 0.9, "top_p": 0.8, "jsonable": True}, "llm_result_1_2",
+             "{\"temperature\": 0.9, \"top_p\": 0.8, \"jsonable\": true}", "llm_result_1_2",
              "fail", 1.0, 0.0, 0.0, 0.0, 100.0],
             ["use_case_2", "scenario_1", "prompt_2_1",
-             {"temperature": 0, "top_p": 0.8, "jsonable": True}, "llm_result_2_1",
+             "{\"temperature\": 0, \"top_p\": 0.8, \"jsonable\": true}", "llm_result_2_1",
              "pass", 0.0, 1.0, 0.0, 100.0, 0.0],
             ["use_case_2", "scenario_2", "prompt_2_2",
-             {"temperature": 0.8, "top_p": 0.9, "jsonable": True}, "llm_result_2_2",
+             "{\"temperature\": 0.8, \"top_p\": 0.9, \"jsonable\": true}", "llm_result_2_2",
              "unknown", 0.0, 0.0, 1.0, 0.0, 0.0]
         ]
         expected_df = pd.DataFrame(data=expected_data, columns=expected_columns)
@@ -158,8 +191,8 @@ class TestGenerateReportFromTestingDetails:
     def test_given_dataframe_should_extract_keys_to_new_columns_if_it_is_json_string(self):
         original_columns = ["column1", "column2"]
         original_data = [
-            ("row1", {"key1": "value11", "key2": "value21"}),
-            ("row2", {"key1": "value12", "key2": "value22"}),
+            ("row1", "{\"key1\": \"value11\", \"key2\": \"value21\"}"),
+            ("row2", "{\"key1\": \"value12\", \"key2\": \"value22\"}"),
         ]
         original_df = pd.DataFrame(data=original_data, columns=original_columns)
 
@@ -173,3 +206,78 @@ class TestGenerateReportFromTestingDetails:
         actual_result_df = report.unnest_json_values(original_df, "column2")
 
         assert actual_result_df.equals(expected_df)
+
+    def test_group_dataframe(self):
+        expected_columns = ["use_case", "scenario", "prompt", "params", "llm_result", "test_result",
+                            "fail", "pass", "unknown", "pass percentage", "fail percentage"]
+        expected_data = [
+            ["use_case_1", "scenario_1", "prompt_1_1",
+             "{\"temperature\": 0, \"top_p\": 0.7, \"jsonable\": true}", "llm_result_1_1",
+             "pass", 0.0, 1.0, 0.0, 100.0, 0.0],
+            ["use_case_1", "scenario_2", "prompt_1_2",
+             "{\"temperature\": 0.9, \"top_p\": 0.8, \"jsonable\": true}", "llm_result_1_2",
+             "fail", 1.0, 0.0, 0.0, 0.0, 100.0],
+            ["use_case_2", "scenario_1", "prompt_2_1",
+             "{\"temperature\": 0, \"top_p\": 0.8, \"jsonable\": true}", "llm_result_2_1",
+             "pass", 0.0, 1.0, 0.0, 100.0, 0.0],
+            ["use_case_2", "scenario_2", "prompt_2_2",
+             "{\"temperature\": 0.8, \"top_p\": 0.9, \"jsonable\": true}", "llm_result_2_2",
+             "unknown", 0.0, 0.0, 1.0, 0.0, 0.0]
+        ]
+        expected_df = pd.DataFrame(data=expected_data, columns=expected_columns)
+
+        grouped_df = expected_df.groupby(["scenario", "use_case"]).sum()
+
+        # Write the grouped DataFrame to an Excel file
+        output_file = "output.xlsx"
+        with pd.ExcelWriter(output_file) as writer:
+            grouped_df.to_excel(writer, sheet_name="Grouped Data", index=True)
+
+        print(f"DataFrame has been written to {output_file}.")
+
+    def test_grouping(self):
+        columns = ["use_case", "scenario", "prompt", "params", "llm_result", "test_result",
+                            "fail", "pass", "unknown", "pass percentage", "fail percentage"]
+        data = [
+            ["use_case_1", "scenario_1", "prompt_1_1", "{\"temperature\": 0, \"top_p\": 0.7, \"jsonable\": true}",
+             "llm_result_1_1",
+             "pass", 0.0, 1.0, 0.0, 100.0, 0.0],
+            ["use_case_1", "scenario_2", "prompt_1_2", "{\"temperature\": 0.9, \"top_p\": 0.8, \"jsonable\": true}",
+             "llm_result_1_2",
+             "fail", 1.0, 0.0, 0.0, 0.0, 100.0],
+            ["use_case_2", "scenario_1", "prompt_2_1", "{\"temperature\": 0, \"top_p\": 0.8, \"jsonable\": true}",
+             "llm_result_2_1",
+             "pass", 0.0, 1.0, 0.0, 100.0, 0.0],
+            ["use_case_2", "scenario_2", "prompt_2_2", "{\"temperature\": 0.8, \"top_p\": 0.9, \"jsonable\": true}",
+             "llm_result_2_2",
+             "unknown", 0.0, 0.0, 1.0, 0.0, 0.0]
+        ]
+        df = pd.DataFrame(data=data, columns=columns)
+
+        # Group the DataFrame by "scenario" and "use_case"
+        grouped_df = df.groupby(["scenario", "use_case"]).sum().sort_index()
+
+        expected_grouped_columns = ["prompt", "params", "llm_result", "test_result",
+                                    "fail", "pass", "unknown", "pass percentage", "fail percentage"]
+        expected_grouped_data = [
+            ["prompt_1_1", "{\"temperature\": 0, \"top_p\": 0.7, \"jsonable\": true}", "llm_result_1_1",
+             "pass", 0.0, 1.0, 0.0, 100.0, 0.0],
+            ["prompt_2_1", "{\"temperature\": 0, \"top_p\": 0.8, \"jsonable\": true}", "llm_result_2_1",
+             "pass", 0.0, 1.0, 0.0, 100.0, 0.0],
+            ["prompt_1_2", "{\"temperature\": 0.9, \"top_p\": 0.8, \"jsonable\": true}", "llm_result_1_2",
+             "fail", 1.0, 0.0, 0.0, 0.0, 100.0],
+            ["prompt_2_2", "{\"temperature\": 0.8, \"top_p\": 0.9, \"jsonable\": true}", "llm_result_2_2",
+             "unknown", 0.0, 0.0, 1.0, 0.0, 0.0]
+        ]
+        index_tuples = [
+            ('scenario_1', 'use_case_1'),
+            ('scenario_1', 'use_case_2'),
+            ('scenario_2', 'use_case_1'),
+            ('scenario_2', 'use_case_2')
+        ]
+        names = ["scenario", "use_case"]
+        index = pd.MultiIndex.from_tuples(index_tuples, names=names)
+        expected_grouped_df = pd.DataFrame(data=expected_grouped_data, columns=expected_grouped_columns,
+                                           index=index).sort_index()
+
+        pd.testing.assert_frame_equal(grouped_df, expected_grouped_df)
