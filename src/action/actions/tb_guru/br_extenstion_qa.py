47e5d960580c4655f3344884012cb68451add2db
danchen <dchen@thoughtworks.com>
2024-03-20 11:07:34 +0800

    [Dan Chen][TG] *N/A feat: ignore previous slots and history for BR extension case

--- src/action/actions/tb_guru/br_extenstion_qa.py
+++ src/action/actions/tb_guru/br_extenstion_qa.py
@@ -68,7 +68,7 @@ search the BR extension
 """
         logger.info(f"search query: {query}")
 
-        response = await self.unified_search.search(SearchParam(query=query), context.conversation.session_id)
+        response = await self.unified_search.search(SearchParam(query=query, tags={"product_line": "BR extension"}), context.conversation.session_id)
         logger.info(f"search response: {response}")
 
         chat_message_preparation = ChatMessagePreparation()
