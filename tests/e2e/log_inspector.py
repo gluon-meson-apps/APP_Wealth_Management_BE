406775c1cb82919eba918fb7307514aff34366bd
ran_lin <boydfd@163.com>
2024-03-27 08:52:42 +0800

    [rlin] *N/A feat: replace cache with memo.

--- tests/e2e/log_inspector.py
+++ tests/e2e/log_inspector.py
@@ -12,16 +12,18 @@ from code_editor import code_editor
 
 st.set_page_config(layout="wide")
 
-@st.cache_data
+
+@st.experimental_memo
 def get_nodes(rootdir):
     nodes = convert_folder_dict_list_like(folder_to_dict(rootdir))
     return nodes
+
 
 # Using "with" notation
 with st.sidebar:
     log_env = st.selectbox("Select log environment", ["bj-3090", "local"])
 
-    db = st.secrets[log_env+ "-db"]
+    db = st.secrets[log_env + "-db"]
 
     for key in db:
         os.environ[key] = db[key]
@@ -40,18 +42,16 @@ the_code = None
 with col1:
     log_id = st.text_input("Enter log id")
 
-
     if st.button("Generate Fix Test"):
         generate_fix_test([log_id])
     st.write(f"Fix test {log_id} generated")
     if return_select and return_select['checked']:
-        with open(root_folder+"/"+return_select['checked'][-1], "r") as f:
+        with open(root_folder + "/" + return_select['checked'][-1], "r") as f:
             the_code = code_editor(f.read())
             st.write(the_code)
             if the_code["type"] == "submit":
-                with open(root_folder+"/"+return_select['checked'][-1], "w") as wf:
+                with open(root_folder + "/" + return_select['checked'][-1], "w") as wf:
                     wf.write(the_code["text"])
-
 
 package = 'tests.e2e.generated'
 with col2:
@@ -62,6 +62,6 @@ with col2:
         #     st.write(module.get_params())
 
         # return use_case, scenario, chat_params["text"], json.dumps(params), result, ""
-        st.write("result: "+ asyncio.run(module.main())[4])
+        st.write("result: " + asyncio.run(module.main())[4])
         # else:
         #     raise Exception(f"The module {module_name} does not have a get_params function or it's not callable")
